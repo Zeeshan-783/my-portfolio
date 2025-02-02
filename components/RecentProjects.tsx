bea23909e3fa935a3741cc30b77add1b6408b3a3
Zeeshan Khalid <khalidzeeshan783@gmail.com>
2025-02-02 09:28:24 -0800

    Updated the components and fixed styling issues

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,25 +13,28 @@ const RecentProjects = () => {
 
         </h1>
 
-        <div className='flex flex-wrap justify-center items-center p-4 gap-x-24 gap-y-24 mt-10'>
+        <div className='flex flex-wrap justify-center items-center p-4 gap-x-24 gap-y-8 mt-10'>
             {projects.map(({
                 id ,title ,des, img ,iconLists , link})=>(
-                <div key={id} className='lg:min-h-[32.5rem] h-[32rem]  flex items-center justify-center sm:w-[570px] w-[80vw]
+                <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem]  flex items-center justify-center sm:w-[570px] w-[80vw]
                 
                 '>
                     <PinContainer title={link} href={link} >
-                    <div className="relative flex items-center justify-center  sm:w-[90vw] w-[80vw] overflow-hidden h-[60vh] sm:h-[70vh] lg:h-[50vh] lg:w-[35vw]">
-                {/* Background Image Container */}
-                <div className="relative overflow-hidden rounded-3xl bg-[#13162d] w-full h-full">
-                  <img src="/bg.png" alt="bg/img" className="w-full h-full object-cover" />
+                    <div className="relative flex items-center justify-center sm:w-[570px] sm:h-[40vh] w-[80vw] overflow-hidden h-[30vh]  mb-10">
+                <div
+                  className="relative w-full h-full overflow-hidden rounded-3xl "
+                  style={{ backgroundColor: "#13162D" }}
+                >
+                  <img src="/bg.png" alt="bgimg" />
                 </div>
-
-                {/* Main Image */}
                 <img
-                  className="z-10 absolute object-contain w-[60vw] h-[50vh] sm:w-[50vw] sm:h-[50vh] lg:w-[35vw] lg:h-[50vh] transform skew-x-12 rounded-2xl"
-                  src={img} alt={img}
+                  src={img}
+                  alt="cover"
+                  className="z-10 absolute  object-fill w-full h-full p-1 rounded-3xl "
                 />
               </div>
+
+
                         <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                             {title}
                         </h1>
